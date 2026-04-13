23d8737423619ad9bdc60e582370e8118c33bde4
jocosa71 <jocosa71@live.com>
2026-04-13 09:50:57 +0200

    Add code to summarize penguins data

---
+++ data summary.R
@@ -0,0 +1,24 @@
+library(tidyverse)
+library(janitor)
+library(palmerpenguins)
+
+data("penguins")
+penguins_raw2 <- clean_names(penguins)
+
+# summarize data per species and island
+penguins_raw2 %>% 
+  mutate(across(c(species, island), factor)) %>% 
+  group_by(species, island) %>%
+  count() %>% 
+  ungroup()
+
+penguins_raw2 %>% 
+  group_by(species, sex) %>% 
+  count()
+
+penguins_raw2 %>% 
+  group_by(species) %>% 
+  summarize(
+    mean_mass = mean(body_mass_g, na.rm = TRUE),
+    sd_mass = sd(body_mass_g, na.rm = TRUE)
+  )
